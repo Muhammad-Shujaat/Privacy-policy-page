1f52cbb2f2b096f892889244954b34d6cad9c1e2
Muhammad-Shujaat <mshujaat97@gmail.com>
2024-04-30 02:14:55 +0500

    final version

---
+++ components/AppPolicies.vue
@@ -0,0 +1,184 @@
+<template>
+    <div class="w-full">
+        
+        <h1 class="text-[1.2rem] sm:text-[1.5rem]  font-bold  p-5">Privacy Policy of ABCAMI Asset</h1>
+        <p class="text-[1.2rem] sm:text-[1.5rem] font-bold p-5">Last Updated- April 25, 2024</p>
+
+        <div class="div-size">
+            <p class="p">Thank you for using ABCAMI Asset and Inventory Management System Mobile app.
+                ABCAMI Asset and Inventory Management System is a leading asset and inventory management system used for
+                management of assets and inventory in companies and organizations. It is owned and operated by ABCAMI
+                Systems.</p>
+
+        </div>
+        <div class="div-size">
+            <p class="p">This Privacy Policy applies to all users or visitors under the publisher's name of
+                “ABCAMI Systems”. We have created this policy to explain our privacy practices, so you can understand
+                what
+                information about you is collected, used and disclosed. We collect information from you in order to
+                provide
+                a corresponding service and a better user experience. With your consent of this Privacy Policy, your
+                usage,
+                statistics, input while using our services would be collected.</p>
+
+        </div>
+        <div class="div-size">
+            <p class="p">Your privacy is important to us, and we are committed to protecting the privacy of parents and
+                children. Please take a moment to familiarize yourself with our privacy practices.</p>
+
+        </div>
+        <div class="div-size">
+            <p class="p">ABCAMI Systems uses and makes all attempts to protect all information when you use our
+                services. If you do not agree with this Privacy Policy, please do not use the service. We reserve the
+                right
+                to make changes to this Privacy Policy. If we make a material change to this Privacy Policy, we will
+                notify
+                you by posting the change in this Privacy Policy.</p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">Information we collected</h4>
+            <p class="p">Generally, to provide you with our Products and Services, we must collect and process
+                information about
+                you. When you sign up or create an account or use our App, we automatically collect the following type
+                of information-
+            </p>
+            <ul class="list-disc ml-7 mt-5 font-light">
+                <li>Network</li>
+                <li>GPS</li>
+                <li>Camera</li>
+                <li>Phone</li>
+                <li>Cookies</li>
+            </ul>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">How we use your personal data</h4>
+            <p class="p">We take care all of your data, so we decided that we only store, required details such as
+                location, phone
+                number, ID. 
+                We do not store or sell parties data to parties.
+                Personal Data Collected
+                We use technologies like cookies on our app, web beacons, or unique device identifiers to anonymously
+                identify your computer or device. Our systems also log information like your browser, operating system,
+                IP address and anonymous usage data, so we can deliver a better experience.
+                We do not sell, trade, or rent user’s personal identification and any data information to others.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap ">Non-personal Data</h4>
+            <p class="p">For purposes of this Privacy Policy, non-personal data means information that does not directly
+                identify
+                you. Additionally, non-personal data means aggregate and de-personalized information, which is data
+                collected about the use of users, from which any personally identifiable data has been removed.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">Device Information</h4>
+            <p class="p">We collect information from and about the computers, phones, and other web-connected devices
+                you use
+                that integrate with our products, and we combine this information with different devices you use. For
+                example, the device is to send push notifications.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">Cookies Policy</h4>
+            <p class="p">We may use cookies (a small text file placed on your device to identify your device and browser
+                when you
+                use our Products & Services). We may also use Web beacons (an electronic file placed on a website that
+                monitors usage). Most Internet browsers automatically accept cookies, but you can instruct your browser
+                to stop accepting cookies (disable cookies). If you do, some of Our Products and Services may not
+                function properly or may be diminished. Promotions or advertisements viewed through Our Products and
+                Services may contain cookies.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">User Agreements</h4>
+            <p class="p">This app, including without limitation all copyrights, patents, trademarks, trade secrets and
+                other
+                intellectual property rights are, and shall remain, the sole and exclusive property of the Company. For
+                the purposes of this End-User License Agreement:
+                “Agreement means” this End-User License Agreement that forms the entire agreement between you and the
+                Company regarding the use of the web.
+            </p>
+            <p class="p">
+                “User Agreement” means Better Health doesn’t allow their users to use their equipment such as company
+                name, company logo, registration number and anything available on this App. 
+            </p>
+            <p class="p">
+                “Company” (referred to as either the Company “We” or “Us”, “Our” in this Agreement) refers to ABCAMI
+                Systems.
+            </p>
+            <p class="p">
+                “Content” refers to content such as text, images, or other information that can be posted, uploaded,
+                linked to or otherwise made available by you, regardless of the form of that content.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">EU user consent policy</h4>
+            <p class="p">From Feb, 2024 if your agreement with Google incorporates this policy, or you otherwise use a
+                Google
+                product that incorporates this policy, you must ensure that certain disclosures are given to, and
+                consents obtained from, end users in the European Economic Area along with the UK. If you fail to comply
+                with this policy, we may limit or suspend your use of the Google product and/or terminate your
+                agreement.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">ADVERTISEMENTS</h4>
+            <p class="p">The app will on occasions also make available to its users/members information about other
+                websites,
+                apps, services, promotions, and contests. By acting on any such information provided by the App, you
+                specifically release the Company from any and all damages and liability, direct, indirect, incidental,
+                and/or consequential. Arising out of your actions based on the information provided, including, without
+                limitation, economic impact, physical damage, bodily injury, and or emotional distress and discomfort.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">Copyright</h4>
+            <p class="p">The copyright and other intellectual property rights in all material materials and content on
+                this App,
+                including but not limited to photographs, graphics, videos, and organization and layout of the App is
+                owned by the Company. In accessing the App, you agree that you may only access the content for your own
+                personal, non-commercial use. Any copies of these pages saved to your computer or any electronic device
+                or disc or removable drive or any other storage medium may be used only for subsequent viewing purposes
+                and for non-commercial personal use only. Except where expressly stated otherwise, you are not permitted
+                to copy, broadcast, download, store (in any medium), transmit, show or play in public, adapt or change
+                in any way the content or any material of the App or its pages for any other purpose whatsoever without
+                the prior written permission of the Company. Use for any other purpose is expressly prohibited by law
+                and may result in severe civil and criminal penalties. Violators will be prosecuted to the maximum
+                extent possible. All rights reserved. Any rights not expressly granted herein are reserved.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">Links to Other Sites</h4>
+            <p class="p">This service may contain links to other sites. If you click on a third-party link, you will be
+                directed
+                to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you
+                to review the Privacy Policy of these websites. We have no control over and assume no responsibility for
+                the content, privacy policies, or practices of any third-party sites or services.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">Children’s Privacy</h4>
+            <p class="p">These services do not address anyone under the age of 13. We do not knowingly collect
+                personally
+                identifiable information from children under 13. In the case we discover that a child under 13 has
+                provided us with personal information, we immediately delete it from our servers. If you are a parent or
+                guardian, and you are aware that your child has provided us with personal information, please contact us
+                so that we will be able to do the necessary actions. Changes to this privacy policy The Owner reserves
+                the right to make changes to this privacy policy at any time by notifying its Users on this page and
+                possibly within this App as far as technically and legally feasible - sending a notice to Users via any
+                contact information available to the Owner. It is strongly recommended to check this page often,
+                referring to the date of the last modification listed at the bottom.
+            </p>
+        </div>
+        <div class="div-size">
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap">Contacting</h4>
+            <p class="p">For questions or comments or changing or deleting your personal information, please do not
+                hesitate to
+                Contact Us at
+            </p>
+            <h4 class="text-[1.1rem] sm:text-[1.325rem] font-[700] text-wrap"> Write your email Id</h4>
+        </div>
+
+    </div>
+</template>
